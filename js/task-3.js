b78baf2478667093264696b7e9a4111b92caa59b
PandaTrumpet <dsavontrumpet@gmail.com>
2023-12-14 20:53:36 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,5 @@
 'use strict'
-// function getElementWidth(content, padding, border) {
-//     content = Number.parseFloat(content);
-//     padding = Number.parseFloat(padding);
-//     border = Number.parseFloat(border);
-//     const elementAllWidth = content + (padding * 2) + (border * 2)
-//     return elementAllWidth;
-// }
 
-// let elementAllWidth = getElementWidth;
-// console.log(elementAllWidth("50px", "8px", "4px" ));
-// console.log(elementAllWidth("60px", "12px", "8.5px"));
-// console.log(elementAllWidth("200px", "0px", "0px"))
 
 
 function checkForSpam(message) {
